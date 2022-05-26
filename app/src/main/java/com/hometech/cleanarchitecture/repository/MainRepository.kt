964a52d97039ccc45145b6949c436c2e96d88d9a
Arpit Patel <arpit@temi.ca>
2022-05-26 15:32:15 -0400

    Enter and Exit animation in Navigation Component

--- app/src/main/java/com/hometech/cleanarchitecture/repository/MainRepository.kt
+++ app/src/main/java/com/hometech/cleanarchitecture/repository/MainRepository.kt
@@ -3,6 +3,7 @@ package com.hometech.cleanarchitecture.repository
 import com.google.gson.Gson
 import com.hometech.cleanarchitecture.cloud.APIServices
 import com.hometech.cleanarchitecture.pojo.CarList
+import com.hometech.cleanarchitecture.pojo.Listings
 import com.hometech.cleanarchitecture.utils.ApiResponse
 
 
@@ -16,5 +17,18 @@ class MainRepository(private val apiServices: APIServices) {
         )
         return ApiResponse.Success(carList)
     }
+
+
+    suspend fun getListing(id: String): ApiResponse<Listings>? {
+        val result = apiServices.getCarList()
+        val carList = result.body() ?: return ApiResponse.Error(result.message().toString())
+
+        for (item in carList.listings) {
+            if (item.id == id)
+                return ApiResponse.Success(item)
+        }
+        return null
+    }
+
 
 }
--- app/src/main/java/com/hometech/cleanarchitecture/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/hometech/cleanarchitecture/viewmodel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hometech.cleanarchitecture.pojo.CarList
+import com.hometech.cleanarchitecture.pojo.Listings
 import com.hometech.cleanarchitecture.repository.MainRepository
 import com.hometech.cleanarchitecture.utils.ApiResponse
 import kotlinx.coroutines.Dispatchers
@@ -13,13 +14,19 @@ import kotlinx.coroutines.launch
 class MainViewModel(mainRepository: MainRepository) : ViewModel() {
 
     private val _carList: MutableLiveData<ApiResponse<CarList>> = MutableLiveData()
+    private val _listing: MutableLiveData<ApiResponse<Listings>> = MutableLiveData()
 
     val carList: LiveData<ApiResponse<CarList>> get() = _carList
+    val listing: LiveData<ApiResponse<Listings>> get() = _listing
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
             _carList.postValue(mainRepository.getCarList())
         }
     }
+
+//    fun getListing(id: String): ApiResponse<Listings>? {
+//        return _listing.postValue()
+//    }
 
 }
