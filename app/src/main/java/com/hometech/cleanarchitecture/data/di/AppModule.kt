2abe35e3dd95093d27d806f0cbaa226f3fcee723
Arpit Patel <arpit@temi.ca>
2022-06-07 12:37:54 -0400

    @Named : When we have same return Type in multiple method than we should use this annotation and specify value for it for later access. See example for detail.
    In our Module
    @Provide
    @Named("base_url")
    
    Access within module
    fun provideRetrofit(client: OkHttpClient.Builder, @Named("base_url") baseUrl: String)
    
    access out side dagger framework (Activity/Fragment/Class)
    
    @Inject
    @Named("base_url")
    val baseUrl:String

--- app/src/main/java/com/hometech/cleanarchitecture/data/di/AppModule.kt
+++ app/src/main/java/com/hometech/cleanarchitecture/data/di/AppModule.kt
@@ -3,20 +3,52 @@ package com.hometech.cleanarchitecture.data.di
 import com.hometech.cleanarchitecture.data.api.APIServices
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 
 @Module
 class AppModule {
-     val BASE_URL: String = "https://carfax-for-consumers.firebaseio.com/"
 
     @Provides
-    fun provideAPIService(): APIServices {
+    @Named("base_url")
+    fun provideBaseURL(): String {
+        return "https://carfax-for-consumers.firebaseio.com/"
+    }
+
+    @Provides
+    @Named("base_url_amazon")
+    fun provideAmazoneBaseUrl():String{
+        return "https://amazon.ca"
+    }
+
+    @Provides
+    fun provideInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+
+        interceptor.apply { interceptor.level = HttpLoggingInterceptor.Level.BODY }
+        return interceptor
+    }
+
+    @Provides
+    fun provideClient(interceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
+        return OkHttpClient.Builder().addInterceptor(interceptor)
+    }
+
+    @Provides
+    fun provideRetrofit(client: OkHttpClient.Builder, @Named("base_url") baseUrl: String): Retrofit {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        return retrofit
+    }
 
+    @Provides
+    fun provideAPIService(retrofit: Retrofit): APIServices {
         return retrofit.create(APIServices::class.java)
     }
 }
